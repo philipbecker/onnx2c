414b3626e18677e2f179d14cb90a89e28a1150f7
Philip Becker-Ehmck <philip.becker-ehmck@argmax.ai>
2023-06-13 17:01:12 +0200

    working split for 1d and 2d variable parts

--- src/nodes/split.h
+++ src/nodes/split.h
@@ -9,11 +9,12 @@ class Split : public Node {
 	public:
 	Split() {
 		op_name = "Split";
-		data=sections=axis=NULL;
+		output=data=sections=axis=NULL;
 	}
 
 	// input and output
-	std::vector<Tensor *> output;
+	// std::vector<Tensor *> output;
+	const Tensor *output;
 	const Tensor *data;
 	const Tensor *sections;
 	const Tensor *axis;
@@ -24,13 +25,12 @@ class Split : public Node {
 
 	virtual void parseAttributes( onnx::NodeProto &node ) override {
 		for( const auto& a : node.attribute() ) {
-			LOG(TRACE) << "Parsing attribute " << a.name() << std::endl;
-			if( a.name() == "sections" )
+			if( a.name() == "split" )
 				_sections = parse_attribute_ints(a);
 			else if( a.name() == "axis" )
 				_axis = parse_attribute_int(a);
 			else
-				ERROR("Unknonw attribute to split");
+				ERROR("Unknown attribute to split");
 		}
 	}
 
@@ -41,7 +41,7 @@ class Split : public Node {
 
 		if (inputs.size() > 1) {
 			sections = inputs[1];
-			register_input(sections, "sections");
+			register_input(sections, "split");
 		}
 
 		if (inputs.size() > 2) {
@@ -49,20 +49,14 @@ class Split : public Node {
 			register_input(axis, "axis");
 		}
 
-		if( sections && sections->isConst == false )
-			ERROR("Non-const inputs to Slice not handled");
-
 		// the output tensor
-		size_t i = 0;
-		for (auto s : _sections)
+		for (int i = 0; i < sections->data_num_elem(); i++)
 		{
 			Tensor *t = new Tensor;
 			t->data_type = data->data_type;
-			auto end = i + s;
-			for (; i < end; i++) {
-				data->get_data_element(i);
-			}
-			register_output(t, "split");
+			t->data_dim = data->data_dim;
+			t->data_dim[_axis] = sections->get_data_element(i);
+			register_output(t, "output" + std::to_string(i));
 		}
 	}
 
@@ -71,7 +65,41 @@ class Split : public Node {
 	{
 
 		INDT_1 << "/* Split */" << std::endl;
+		INDT_1 << "/* axis: " << _axis << " */" << std::endl;
 
+		int64_t s = 0;
+		for (int i = 0; i < sections->data_num_elem(); i++) 
+		{
+			INDT_1 << "/* Split " << std::to_string(i) <<  " of size " << std::to_string(sections->get_data_element(i)) << " */" << std::endl;
+			std::string out_idx = std::to_string(i);
+			std::string in_idx;
+
+			for (unsigned d = 0; d < data->rank(); d++)
+			{
+				std::string iv = "i" + std::to_string(d);
+				std::string ov = "o" + std::to_string(d);
+				if (d == _axis) {
+					INDT(d + 1) << "for (unsigned " << iv << "=" << s << ", " << ov << "=0; ";
+					dst << ov << "<" << sections->get_data_element(i) << "; ";
+				} else {
+					INDT(d + 1) << "for (unsigned " << iv << "=" << 0 << ", " << ov << "=0; ";
+					dst << ov << "<" << data->data_dim[d] << "; ";
+				}
+				
+				dst << iv << "++, " << ov << "++) {" << std::endl;
+
+				out_idx += "[" + ov + "]";
+				in_idx += "[" + iv + "]";
+			}
+			// Copy over data from input to output
+			INDT(data->rank() + 1) << "output" << out_idx << " = data" << in_idx << ";" << std::endl;
+
+			// close loops over output dimensions
+			for( unsigned r=data->rank(); r>0; r--) {
+				INDT(r) << "}" << std::endl;
+			}
+			s += sections->get_data_element(i);
+		}
 	}
 };
 }
